e8cf477c7e93689565198109f03d5cae5c260021
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-10 23:50:08 +0530

    adding data.sql and data initialization property

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple, port) values (1000L, 'USD', 'INR', 65, 0);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple, port) values (1001L, 'EUR', 'INR', 75, 0);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple, port) values (1002L, 'AUD', 'INR', 25, 0);
